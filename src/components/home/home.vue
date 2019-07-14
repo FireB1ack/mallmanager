2323043f6c2223a5bbf8db6865e6107c7c4d8382
fireblack <chenghao855@gmail.com>
2019-07-14 15:25:29 +0800

    用户退出功能

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -14,7 +14,7 @@
         </el-col>
         <el-col :span="2">
           <div class="grid-content bg-purple">
-            <a href="#" class="loginout">退出</a>
+            <a href="#" @click.prevent="handleLoginOut()" class="loginout">退出</a>
           </div>
         </el-col>
       </el-row>
@@ -22,8 +22,8 @@
     <el-container>
       <el-aside width="200px" class="container-aside">
         <el-menu unique-opened="true" router="true"
-          default-active="2"
-          class="el-menu-vertical-demo">
+                 default-active="2"
+                 class="el-menu-vertical-demo">
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-user-solid"></i>
@@ -58,9 +58,9 @@
               <span slot="title">商品列表</span>
             </el-menu-item>
             <el-menu-item index="1-2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">分类参数</span>
-          </el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span slot="title">分类参数</span>
+            </el-menu-item>
             <el-menu-item index="1-3">
               <i class="el-icon-menu"></i>
               <span slot="title">商品分类</span>
@@ -98,9 +98,28 @@
     beforeCreate() {
       //进行首页权限验证，如果不通过重新登陆
       const token = localStorage.getItem("token");
-      if(!token){
-        this.$router.push({name:'login'});
+      if (!token) {
+        this.$router.push({name: 'login'});
         this.$message.warning('请先登陆')
+      }
+    },
+    methods: {
+      handleLoginOut() {
+        this.$confirm('是否确认退出该用户?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'info'
+        }).then(() => {
+          //用户登出
+          localStorage.clear();
+          this.$router.push({name: 'login'});
+          this.$message.success('用户退出成功');
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消退出'
+          });
+        });
       }
     }
   }
